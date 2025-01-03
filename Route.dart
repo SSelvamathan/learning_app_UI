26bde41a96ab27ccd4e23d622052abc306f6c4d1
SSelvamathan <selvamathan07@gmail.com>
2025-01-03 08:06:33 -0800

    Update Route.dart

--- Route.dart
+++ Route.dart
@@ -1,1 +1,17 @@
+import 'package:get/get.dart';
+import 'package:new_project_getX/pages/home_page/homepage.dart';
+import 'package:new_project_getX/pages/home_page/homepage_for_UI_1.dart';
+import 'package:new_project_getX/pages/nextpage/nextpage1.dart';
+import 'package:new_project_getX/pages/nextpage/page2_UI.dart';
+
+appRoutes() => [
+      GetPage(
+          name: '/home',
+          page: () => HomePageUI(),
+          transition: Transition.leftToRight),
+      GetPage(
+        name: '/nextpage',
+        page: () => PageUI_2(),
+      )
+    ];
 
