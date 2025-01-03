547df8276030cfc6733f88ed45a7fe619025af7e
SSelvamathan <selvamathan07@gmail.com>
2025-01-03 10:18:47 -0800

    Update Route.dart

--- Route.dart
+++ Route.dart
@@ -3,6 +3,7 @@ import 'package:new_project_getX/pages/home_page/homepage.dart';
 import 'package:new_project_getX/pages/home_page/homepage_for_UI_1.dart';
 import 'package:new_project_getX/pages/nextpage/nextpage1.dart';
 import 'package:new_project_getX/pages/nextpage/page2_UI.dart';
+import 'package:new_project_getX/pages/nodificationpage_UI/nodifyPage_UI.dart';
 
 appRoutes() => [
       GetPage(
@@ -12,6 +13,7 @@ appRoutes() => [
       GetPage(
         name: '/nextpage',
         page: () => PageUI_2(),
-      )
+      ),
+      GetPage(name: '/nodifyPage', page: () => NodifyPage3_UI()),
+      // GetPage(name: '/nextpage', page: () => PageUI_2())
     ];
-
